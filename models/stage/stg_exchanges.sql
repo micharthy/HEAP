6af991130387e9cc490fc08d187cee443849b3f2
Michael Mc Carthy <ganstack@gmail.com>
2024-05-27 14:21:50 +0000

    setup temp schemas for int and staging

---
+++ models/stage/stg_exchanges.sql
@@ -0,0 +1,38 @@
+with 
+
+source as (
+
+    select * from {{ source('source', 'exchanges') }}
+
+),
+
+renamed as (
+
+    select
+        conversation_id,
+        exchange_id,
+        created_at,
+        creator_id,
+        guest_user_id,
+        host_user_id,
+        finalized_at,
+        canceled_at,
+        start_on,
+        end_on,
+        guest_countguest_count,
+        night_count,
+        user_cancellation_id,
+        exchange_type,
+        home_type,
+        residence_type,
+        capacity,
+        country,
+        region,
+        department,
+        city
+
+    from source
+
+)
+
+select * from renamed
