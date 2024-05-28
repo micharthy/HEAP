79c2b6ab5269be0585f6f3b1d2a745958113ac85
Michael Mc Carthy <ganstack@gmail.com>
2024-05-28 16:03:44 +0000

    fab fab

--- models/int/int_host_exg.sql
+++ models/int/int_host_exg.sql
@@ -2,26 +2,35 @@ with
 
 exchanges as (
 
-    select * from {{ ref('stg_exchanges')}}
+    select * from {{ ref('int_exchanges')}}
+
+),
+--- filtering out exchanges where the creator is the host as this would only be the case for reciprocal exchanges (where multiple rows are created for 1 exchange)
+host_non_creator as (
+    
+    select *
+    from exchanges
+    where creator_id <> host_user_id
 
 )
 
 
 select
   distinct(host_user_id) as host_id,
+  min(created_at) as first_host_reqst,
+  max(created_at) as last_host_reqst,
   count(*) as nb_reqst,
-  min(created_at) as first_reqst,
-  max(created_at) as last_reqst,
   sum(night_count) as nb_nights_reqst,
-  avg(night_count) as avg_night_count,
-  countif(finalized_at IS NULL) as nb_exg_not_finalized,
-  countif(finalized_at IS NOT NULL AND canceled_at IS NULL) as nb_exg_finalized_not_cancelled,
-  countif(finalized_at IS NOT NULL AND canceled_at IS NOT NULL) as nb_exg_finalized_cancelled,
+  CAST(round(avg(night_count),0) AS INT) as avg_night_count,
+  countif(exg_status = 'SUCCESS') as nb_success_exg,
+  sum(if(exg_status = 'SUCCESS', night_count, 0)) as sum_nights_success_reqst,
+  countif(exg_status = 'FAIL') as nb_failed_exg,
   countif(exchange_type = 'NON_RECIPROCAL') as nb_non_reciprocal_exg,
   countif(exchange_type = 'RECIPROCAL') as nb_reciprocal_exg,
   min(capacity) as min_capacity,
   max(capacity) as max_capacity,
-  avg(capacity) as avg_capactiy,
-  countif(distinct city IS NOT NULL) as nb_cities
-from exchanges
+  CAST(round(avg(capacity),0) AS INT) as avg_capactiy,
+  count(distinct country) as nb_countries,
+  count(distinct city) as nb_cities
+from host_non_creator
 group by host_id
